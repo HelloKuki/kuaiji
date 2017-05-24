18560989e019229a311a08b02726e6eb3b6cd9bc
huangqiyu <15627537013@163.com>
2017-05-24 17:56:39 +0800

    [feature]又写了点代码

--- app/src/main/java/com/hellokuki/kuaiji/adapter/MainRecyclerViewAdapter.java
+++ app/src/main/java/com/hellokuki/kuaiji/adapter/MainRecyclerViewAdapter.java
@@ -12,17 +12,28 @@ import com.hellokuki.kuaiji.R;
  * Created by huangqiyu on 2017/5/23.
  */
 public class MainRecyclerViewAdapter extends RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder> {
+    public final int NORMAL = 1;
+    public final int FOOTER = 2;
+
 
     private LayoutInflater mInflater;
+    private View mFooterView;
+
 
     public MainRecyclerViewAdapter(Context context) {
         mInflater = LayoutInflater.from(context);
     }
 
+    public void addFooterView(View footerView) {
+        mFooterView = footerView;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        ViewHolder holder = new ViewHolder(mInflater.inflate(R.layout.layout_bill_item, parent, false));
-        return holder;
+        if (mFooterView != null && viewType == FOOTER) {
+            return new ViewHolder(mFooterView);
+        }
+        return new ViewHolder(mInflater.inflate(R.layout.layout_bill_item, parent, false));
     }
 
     @Override
@@ -31,15 +42,30 @@ public class MainRecyclerViewAdapter extends RecyclerView.Adapter<MainRecyclerVi
     }
 
     @Override
+    public int getItemViewType(int position) {
+        if (mFooterView == null) {
+            return NORMAL;
+        }
+
+        if (position == getItemCount() - 1) {
+            return FOOTER;
+        }
+        return NORMAL;
+    }
+
+    @Override
     public int getItemCount() {
+        if (mFooterView != null) {
+            return 10 + 1;
+        }
         return 10;
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
-
         public ViewHolder(View itemView) {
             super(itemView);
+            if (itemView == mFooterView) return;
         }
     }
 }
--- app/src/main/java/com/hellokuki/kuaiji/view/MainActivity.java
+++ app/src/main/java/com/hellokuki/kuaiji/view/MainActivity.java
@@ -1,8 +1,10 @@
 package com.hellokuki.kuaiji.view;
 
 import android.animation.ObjectAnimator;
+import android.graphics.Rect;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +13,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.hellokuki.kuaiji.R;
@@ -23,10 +27,13 @@ public class MainActivity extends AppCompatActivity {
     private AppBarLayout mAppBarLayout;
     private RelativeLayout mRelativeLayout;
     private RecyclerView mRecyclerView;
+    private FloatingActionButton mButtonAdd;
 
     private MainRecyclerViewAdapter mRecyclerAdapter;
     private ObjectAnimator mAnimatorShow;
     private ObjectAnimator mAnimatorHide;
+    private ObjectAnimator mAnimatorButtonShow;
+    private ObjectAnimator mAnimatorButtonHide;
     private boolean mTitleIsShow = false;
 
     @Override
@@ -37,16 +44,23 @@ public class MainActivity extends AppCompatActivity {
         mAppBarLayout = (AppBarLayout) findViewById(R.id.app_bar_layout);
         mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
         mRelativeLayout = (RelativeLayout) findViewById(R.id.relative_layout_title_content);
-        mRecyclerView= (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mButtonAdd= (FloatingActionButton) findViewById(R.id.button_add);
+
+
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        mRecyclerAdapter=new MainRecyclerViewAdapter(this);
+        mRecyclerAdapter = new MainRecyclerViewAdapter(this);
+        mRecyclerView.addItemDecoration(new SpaceItemDecoration(8));
+        mRecyclerAdapter.addFooterView(LayoutInflater.from(this).inflate(R.layout.layout_bill_item_footer, mRecyclerView, false));
         mRecyclerView.setAdapter(mRecyclerAdapter);
 
         mAppBarLayout.addOnOffsetChangedListener(new OnOffsetListener());
         mRelativeLayout.setAlpha(0f);
         mAnimatorShow = ObjectAnimator.ofFloat(mRelativeLayout, "alpha", 0f, 1f).setDuration(500);
         mAnimatorHide = ObjectAnimator.ofFloat(mRelativeLayout, "alpha", 1f, 0f).setDuration(500);
+        mAnimatorButtonShow = ObjectAnimator.ofFloat(mButtonAdd, "alpha", 0f, 1f).setDuration(500);
+        mAnimatorButtonHide = ObjectAnimator.ofFloat(mButtonAdd, "alpha", 1f, 0f).setDuration(500);
     }
 
     class OnOffsetListener implements AppBarLayout.OnOffsetChangedListener {
@@ -54,13 +68,33 @@ public class MainActivity extends AppCompatActivity {
         public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
             if (!mTitleIsShow && Math.abs(verticalOffset) > appBarLayout.getTotalScrollRange() / 1.63) {
                 mAnimatorShow.start();
+                mAnimatorButtonHide.start();
                 mTitleIsShow = true;
             }
             if (mTitleIsShow && Math.abs(verticalOffset) < appBarLayout.getTotalScrollRange() / 1.63) {
                 mAnimatorHide.start();
+                mAnimatorButtonShow.start();
                 mTitleIsShow = false;
             }
 
+        }
+    }
+
+    public class SpaceItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int space;
+
+        public SpaceItemDecoration(int space) {
+            this.space = space;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            if (parent.getChildPosition(view) != 0) {
+                outRect.top = space;
+            } else {
+                outRect.top = space * 3;
+            }
         }
     }
 
